60efb8fa59e4e4ae357af388f9789d693fbe02be
Aravind Samala <samalaaravind02@gmail.com>
2023-11-17 21:11:06 +0530

    TIDY-255 Logs - thumbnail fixes, session log page init  and focus improvements, fixes

---
+++ elements/button/BackButton.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { Size } from "$lib/tidy/types/size.enum";
+  import Icon from "../Icon.svelte";
+  export let color: string = "";
+</script>
+
+<button class="flex items-center" on:click>
+  <Icon icon="chevleft" size={Size.sm} {color} />
+  <div class="pr-1">Back</div>
+</button>
