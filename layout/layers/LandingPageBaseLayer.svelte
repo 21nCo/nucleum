b14ae7340721af1503dec9d63f20033ee20a0ae1
Aravind Samala <samalaaravind02@gmail.com>
2023-12-24 12:22:35 +0530

    TIDY-373 - logos and coming soon for other sub atoms

--- layout/layers/LandingPageBaseLayer.svelte
+++ layout/layers/LandingPageBaseLayer.svelte
@@ -2,6 +2,7 @@
   import { onMount } from "svelte";
   import ThemeLayer from "./ThemeLayer.svelte";
   import { Persistance } from "$lib/tidy/stores/persistance";
+  import { appStore } from "$lib/tidy/stores/app.store";
   onMount(async () => {
     await initializeAppData();
   });
@@ -12,6 +13,9 @@
   }
 </script>
 
-<ThemeLayer>
-  <slot />
-</ThemeLayer>
+<title>{$appStore.appData.name ?? ""}</title>
+<div class="flex h-screen w-screen">
+  <ThemeLayer>
+    <slot />
+  </ThemeLayer>
+</div>
