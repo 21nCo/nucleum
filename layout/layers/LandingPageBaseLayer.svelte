6644420d77cbd7777f9eaedc1ae434989b7db016
Aravind Samala <samalaaravind02@gmail.com>
2023-12-24 10:47:59 +0530

    TIDY-373 - separating landing page from Pointron

---
+++ layout/layers/LandingPageBaseLayer.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import ThemeLayer from "./ThemeLayer.svelte";
+  import { Persistance } from "$lib/tidy/stores/persistance";
+  onMount(async () => {
+    await initializeAppData();
+  });
+  async function initializeAppData() {
+    const app = import.meta.env.VITE_APP ?? window.location.hostname;
+    if (!app) return;
+    await new Persistance().initializeAppData(app);
+  }
+</script>
+
+<ThemeLayer>
+  <slot />
+</ThemeLayer>
