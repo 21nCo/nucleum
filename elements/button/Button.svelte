59d7d6d1f3246c4e0284918f7ee7bb48b631affb
Fuzail Haaris <fuzailhaariss@gmail.com>
2024-02-02 08:00:37 +0000

    Revert Button.svelte

--- elements/button/Button.svelte
+++ elements/button/Button.svelte
@@ -27,7 +27,7 @@
   $: if (!label && icon && style == ButtonStyle.DEFAULT && !$$slots.default)
     style = ButtonStyle.PLAIN;
   let classList =
-    "inline-flex flex-row justify-center items-center min-w-fit " +
+    "flex flex-row justify-center items-center min-w-fit " +
     (style === ButtonStyle.ROUNDED || size === Size.xs
       ? " rounded-full"
       : " rounded-md") +
