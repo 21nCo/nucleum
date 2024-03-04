89d350625415ad942d72a987cdf21e779d750186
Aravind Samala <samalaaravind02@gmail.com>
2024-03-04 14:12:15 +0530

    MEMO-84 - create curation flow - option selectors

---
+++ elements/switcher/OptionSelector.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+  import { userPreferences } from "$lib/tidy/stores/app.store";
+  import {
+    OptionSelectorStyle,
+    type SwitchItem
+  } from "$lib/tidy/types/switcher.enum";
+  import { bgClass } from "$lib/tidy/utils/theme.utils";
+  import { createEventDispatcher, onMount } from "svelte";
+  import { Size } from "$lib/tidy/types/size.enum";
+  import OptionSelectorItem from "./OptionSelectorItem.svelte";
+  import FormControlLabelWrapper from "../input/FormControlLabelWrapper.svelte";
+  import { Orientation } from "$lib/tidy/types/direction.enum";
+  import type { InfoTextParams } from "$lib/tidy/types/text.type";
+  const dispatch = createEventDispatcher();
+  export let items: SwitchItem[];
+  export let label: string = "";
+  export let selected: string | undefined = undefined;
+  export let parentBackgroundIndex: number = 1;
+  export let size: Size = Size.md;
+  export let style: OptionSelectorStyle = OptionSelectorStyle.TRAIN;
+  export let iconOrientation: Orientation = Orientation.Horizontal;
+  export let info: InfoTextParams | undefined = undefined;
+  let classList: string = " flex w-full ";
+  onMount(() => {
+    if (selected === undefined) selected = items[0].label;
+    switch (style) {
+      case OptionSelectorStyle.TRAIN:
+        classList += " rounded-md "; //+
+        //bgClass($userPreferences.theme, parentBackgroundIndex);
+        if (size != Size.sm) {
+          classList += " border-2 border-brs3 ";
+        }
+        break;
+      case OptionSelectorStyle.OUTLINE:
+        if (size === Size.md) {
+          classList += "gap-4 overflow-auto";
+        } else if (size === Size.sm) {
+          classList += "gap-2 overflow-auto";
+        }
+        break;
+      default:
+        classList += " items-center";
+        break;
+    }
+  });
+</script>
+
+<FormControlLabelWrapper {label} {info}
+  ><div class="relative w-full">
+    <div class={classList}>
+      {#each items as item, index}
+        <OptionSelectorItem
+          {item}
+          {size}
+          {style}
+          orientation={iconOrientation}
+          isActive={selected === item.label}
+          on:click={() => {
+            selected = item.label;
+            dispatch("switch", item);
+          }}
+        />
+      {/each}
+    </div>
+  </div></FormControlLabelWrapper
+>
---
+++ elements/switcher/OptionSelectorItem.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+  import { windowObject } from "$lib/tidy/stores/app.store";
+  import { Orientation } from "$lib/tidy/types/direction.enum";
+  import { Size } from "$lib/tidy/types/size.enum";
+  import {
+    OptionSelectorStyle,
+    type SwitchItem
+  } from "$lib/tidy/types/switcher.enum";
+  import { properCase } from "$lib/tidy/utils/text.utils";
+  import Icon from "../Icon.svelte";
+  export let item: SwitchItem;
+  export let size: Size;
+  export let isActive: boolean = false;
+  export let style: OptionSelectorStyle = OptionSelectorStyle.TRAIN;
+  export let orientation: Orientation = Orientation.Horizontal;
+</script>
+
+{#if style === OptionSelectorStyle.TRAIN || style === OptionSelectorStyle.OUTLINE}
+  <button
+    class="relative rounded-md {style === OptionSelectorStyle.TRAIN
+      ? 'flex grow justify-center outline'
+      : 'border-2'} {orientation === Orientation.Horizontal
+      ? size === Size.xl
+        ? ' px-12 py-8'
+        : size === Size.md
+          ? ' px-8 py-4'
+          : ' px-3 py-1 '
+      : size === Size.xl
+        ? ' px-8 py-6'
+        : size === Size.md
+          ? ' px-6 py-4'
+          : ' px-3 py-1 '} {isActive ? 'activeBgColor' : 'outline-transparent'}"
+    on:click
+  >
+    <div
+      class="flex {orientation === Orientation.Vertical
+        ? 'flex-col gap-1'
+        : 'gap-2'} {size === Size.md && $windowObject.isInPortraitMode
+        ? 'text-base font-medium'
+        : size === Size.sm
+          ? 'text-b2'
+          : 'text-base'}"
+    >
+      {#if item.icon}
+        <Icon icon={item.icon} />
+      {/if}
+      {properCase(item.label)}
+    </div>
+  </button>
+{/if}
+
+<style>
+  .activeBgColor {
+    background-color: var(--customcolor, rgba(var(--colors-aps1), 0.05));
+    outline-color: var(--customcolor, rgba(var(--colors-aps1), 0.4));
+    border-color: var(--customcolor, rgba(var(--colors-aps1), 0.4));
+  }
+</style>
