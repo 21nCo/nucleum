3a043e4809461c6d83404a2aa7368908908156de
Fuzail22 <fuzailhaariss@gmail.com>
2024-10-15 13:06:01 +0530

    PDFTaskSeperation

--- client/products/memotron/node/traces/NodeTracesPane.svelte
+++ client/products/memotron/node/traces/NodeTracesPane.svelte
@@ -108,55 +108,57 @@
 {#if pdfAnnotations.length > 0}
   <div class="h-full w-full flex flex-col gap-2 mt-2 overflow-y-scroll">
     {#each pdfAnnotations as trace, index}
-      <button
-        class="block relative w-full p-2 text-b2 text-left border border-brs3 rounded-md hover:bg-bgs2"
-        on:click={() => {
-          contentContext.publish("pdf-trace-click", {
-            id: trace.id,
-            pageNumber: trace.pageNumber
-          });
-        }}
-      >
-        <!-- TODO - delete, link, edit actions within traces panel -->
-        <!-- <button
+      {#if (selectedType == "tasks" && trace.annotType === AnnotationType.TASK) || (selectedType != "tasks" && trace.annotType !== AnnotationType.TASK)}
+        <button
+          class="block relative w-full p-2 text-b2 text-left border border-brs3 rounded-md hover:bg-bgs2"
+          on:click={() => {
+            contentContext.publish("pdf-trace-click", {
+              id: trace.id,
+              pageNumber: trace.pageNumber
+            });
+          }}
+        >
+          <!-- TODO - delete, link, edit actions within traces panel -->
+          <!-- <button
           on:click|stopPropagation={() => handleAnnotDelete(null, trace.id)}
           class="absolute top-1 right-0 material-symbols-rounded text-base text-fgs4 hover:text-h4 hover:text-fgs2 z-40"
           >{@html "&#Xe92b"}</button
         > -->
-        {#if trace.comment}
-          <div
-            class="flex flex-col gap-2 w-full min-h-fit p-2 rounded-md text-fgs2"
-          >
-            {#if trace.selectedText}
-              <blockquote
-                class="border-l-2 border-orange-400 p-2 opacity-50 text-b3"
-              >
-                {trace.selectedText.slice(0, 100)}
-              </blockquote>
-            {/if}
-            <div>
-              {trace.comment}
+          {#if trace.comment}
+            <div
+              class="flex flex-col gap-2 w-full min-h-fit p-2 rounded-md text-fgs2"
+            >
+              {#if trace.selectedText}
+                <blockquote
+                  class="border-l-2 border-orange-400 p-2 opacity-50 text-b3"
+                >
+                  {trace.selectedText.slice(0, 100)}
+                </blockquote>
+              {/if}
+              <div>
+                {trace.comment}
+              </div>
             </div>
-          </div>
-        {:else}
-          {@const color = highlightStore.resolveColor(trace.color)}
-          <div
-            class="w-full min-h-fit p-2 rounded-md text-fgs2"
-            style="text-decoration: 2px {trace.annotType?.toLowerCase()} {trace.annotType !==
-            AnnotationType.HIGHLIGHT
-              ? color
-              : ''}; "
-          >
-            <span
-              style={trace.annotType === AnnotationType.HIGHLIGHT
-                ? `background-color: ${hexToRGBA(color, 0.2)};`
-                : ""}
+          {:else}
+            {@const color = highlightStore.resolveColor(trace.color)}
+            <div
+              class="w-full min-h-fit p-2 rounded-md text-fgs2"
+              style="text-decoration: 2px {trace.annotType?.toLowerCase()} {trace.annotType !==
+              AnnotationType.HIGHLIGHT
+                ? color
+                : ''}; "
             >
-              {trace.selectedText.slice(0, 100)}</span
-            >
-          </div>
-        {/if}
-      </button>
+              <span
+                style={trace.annotType === AnnotationType.HIGHLIGHT
+                  ? `background-color: ${hexToRGBA(color, 0.2)};`
+                  : ""}
+              >
+                {trace.selectedText.slice(0, 100)}</span
+              >
+            </div>
+          {/if}
+        </button>
+      {/if}
     {/each}
   </div>
 {:else if canHaveTraces.includes($node?.contentType ?? NodeType.UNKNOWN) && selectedType === "clips"}
