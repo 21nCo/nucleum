d0eb86f7c10bf418cfa6666c8ffcd89c8887c1db
Aravind Samala <samalaaravind02@gmail.com>
2024-11-11 12:10:47 +0530

    21n landing page - MEMO-399 - privacy policy update, temp tacoWorker

--- client/landing/blank.coop/+layout.svelte
+++ client/landing/blank.coop/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import BlankLandingLayout from "$lib/client/landing/shared/BlankLandingLayout.svelte";
+  import type { IFooter } from "../shared/Landing.types";
 
   const topNavBarValues = {
     icon: "21n-temp",
@@ -8,8 +9,11 @@
       { label: "White paper", href: "https://papers.21n.io/soft" }
     ]
   };
+  let footerValues: IFooter = {
+    youtubeUrl: "https://www.youtube.com/@21nCompany"
+  };
 </script>
 
-<BlankLandingLayout {topNavBarValues}>
+<BlankLandingLayout {topNavBarValues} {footerValues}>
   <slot />
 </BlankLandingLayout>
--- client/products/memotron/memotron.utils.ts
+++ client/products/memotron/memotron.utils.ts
@@ -36,10 +36,11 @@ export function generateSyncedResourceId(externalId: string, type: NodeType) {
 /**
  * Creates a seprate thread for taco functions to avoid blocking the main thread.
  */
-export const tacoWorker = new Worker(
-  new URL("$lib/client/products/memotron/taco/taco.worker.ts", import.meta.url),
-  { type: "module" }
-);
+// export const tacoWorker = new Worker(
+//   new URL("$lib/client/products/memotron/taco/taco.worker.ts", import.meta.url),
+//   { type: "module" }
+// );
+export let tacoWorker = {};
 
 export function resolveFileUploadErrorMessage(
   errors: { file: File; type: string }[],
