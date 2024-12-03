9b83593123cafd8e13e5f340d13dcdf55ac78a6d
Aravind Samala <samalaaravind02@gmail.com>
2024-12-03 22:01:33 +0530

    less - 86cx7wj28 - delete account scenarios - ping API to return user

--- server/logger.ts
+++ server/logger.ts
@@ -2,7 +2,7 @@ import { performQueryOnMasterDb } from "./surrealHelpers";
 
 export async function log(userId: string, context: any) {
   const timestamp = new Date().toISOString();
-  const query = `create activity set userId = "user:${userId}", timestamp = "${timestamp}", context = ${JSON.stringify(
+  const query = `select id from user:${userId}; create activity set userId = user:${userId}, timestamp = "${timestamp}", context = ${JSON.stringify(
     context
   )}`;
   const response = await performQueryOnMasterDb(query);
