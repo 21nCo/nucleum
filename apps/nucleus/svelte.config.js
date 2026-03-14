8420895e340ca5b4cde047126ab62f28d613cd90
Sriya-Mukkawar <mukkawarsriya@gmail.com>
2026-03-14 13:24:31 +0530

    removed svelte aliases

--- apps/nucleus/svelte.config.js
+++ apps/nucleus/svelte.config.js
@@ -11,32 +11,8 @@ const config = {
     }),
     alias: {
       $local: "./src",
-      $lib: "../../",
-      // @21n/* aliases so SvelteKit does not conflict with tsconfig paths (see kit.alias docs)
-      "@21n/actions": "../../client/actions",
-      "@21n/components": "../../client/components",
-      "@21n/elements": "../../client/elements",
-      "@21n/stores": "../../client/stores",
-      "@21n/utils": "../../client/utils",
-      "@21n/types": "../../client/types",
-      "@21n/products": "../../client/products",
-      "@21n/static": "../../client/static",
-      "@21n/layout": "../../client/layout",
-      "@21n/persistence": "../../client/persistence",
-      "@21n/landing": "../../client/landing",
-      "@21n/extensions": "../../client/extensions",
-      "@21n/branding": "../../client/branding",
-      "@21n/icons": "../../client/icons",
-      "@21n/theme": "../../client/theme",
-      "@21n/legacy": "../../client/legacy",
-      "@21n/data": "../../client/data",
-      "@21n/illustrations": "../../client/illustrations",
-      "@21n/icons-v2": "../../client/iconsV2",
-      "@21n/cx": "../../client/cx",
-      "@21n/client": "../../client",
-      "@21n/shared-types": "../../shared/types",
-      "@21n/shared-utils": "../../shared/utils",
-      "@21n/shared-dbo": "../../shared/dbo"
+      // Point $lib to the parent lib directory (2 levels up)
+      $lib: "../../"
     },
     files: {
       // lib points to the parent lib for this product
