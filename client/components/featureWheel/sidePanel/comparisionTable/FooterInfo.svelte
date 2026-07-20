555cce730f225d2e1e5f6063cb5119037de6c000
Aravind Samala <samalaaravind02@gmail.com>
2026-07-20 10:17:13 +0530

    remove landing - marketing content

--- client/components/featureWheel/sidePanel/comparisionTable/FooterInfo.svelte
+++
@@ -1,4 +0,0 @@
-<span class="text-fgs3 text-b3">
-  * Price is in USD/month billed annually for all core features without limits +
-  cloud sync + a minimum of 10 GB storage. (Excluding AI credits)
-</span>
--- client/components/featureWheel/sidePanel/comparisionTable/NotesPopover.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  import MarkdownRenderer from "@21n/landing/shared/elements/MarkdownRenderer.svelte";
-
-  let { notes }: { notes: string } = $props();
-</script>
-
-<div class="bg-bgs1 p-3 rounded-md border border-brs3">
-  <MarkdownRenderer text={notes} />
-</div>
