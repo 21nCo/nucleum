d46e82e170ecebe55c1526788134be5cc3823b78
Aravind Samala <samalaaravind02@gmail.com>
2026-08-29 12:04:37 +0530

    Remove legacy web data runtime

--- client/components/flux/resourceStores/store.resolver.ts
+++
@@ -1,9 +0,0 @@
-import { kvStores } from "@21n/components/flux/resourceStores/kv.store";
-import type { Resource } from "@21n/components/flux/resourceStores/resource.enum";
-import { resourceStores } from "@21n/components/flux/resourceStores/resource.store";
-
-export function resolveResourceStore(resource: Resource) {
-  const store = resourceStores.get(resource);
-  if (store) return store;
-  return kvStores.get(resource);
-}
